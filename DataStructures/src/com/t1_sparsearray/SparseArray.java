744eaff7753f4a752eb15ef1bb866ed998bb2d9c
董升 <2504816574@qq.com>
2021-02-24 18:26:39 +0800

    稀疏数组写入本地，读取

--- DataStructures/src/com/t1_sparsearray/SparseArray.java
+++ DataStructures/src/com/t1_sparsearray/SparseArray.java
@@ -1,5 +1,7 @@
 package com.t1_sparsearray;
 
+import java.io.*;
+
 /**
  * @author 25048
  * @Date 2021/2/24
@@ -7,7 +9,7 @@ package com.t1_sparsearray;
  * 稀疏数组
  */
 public class SparseArray {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 创建一个原始的二维数组 11 * 11
         // 0: 表示没有棋子， 1 表示 黑子 2 表蓝子
         int[][] chessArr1 = new int[11][11];
@@ -63,6 +65,46 @@ public class SparseArray {
         }
         System.out.println();
 
+
+        //将稀疏数组保存到本地
+        File file = new File("F:\\sparseArr.txt");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        FileWriter fileWriter = new FileWriter(file);
+        for (int[] value : sparseArr) {
+            for (int i : value) {
+                fileWriter.write(i + "\t");
+            }
+            fileWriter.write("\r\n");
+
+        }
+        fileWriter.close();
+
+        // 读取本地文件，生成稀疏数组
+        int[][] sparseArr2=new int[sparseArr.length][3];
+        BufferedReader in = new BufferedReader(new FileReader(file));
+        String line;  //一行数据
+        int row = 0;
+        //逐行读取，并将每个数组放入到数组中
+        while ((line = in.readLine()) != null) {
+            String[] temp = line.split("\t");
+            for (int j = 0; j < temp.length; j++) {
+                sparseArr2[row][j] = Integer.parseInt(temp[j]);
+            }
+            row++;
+        }
+        in.close();
+        System.out.println("以写入:"+file);
+
+
+        System.out.println();
+        System.out.println("本地得到稀疏数组为~~~~");
+        for (int[] ints : sparseArr2) {
+            System.out.printf("%d\t%d\t%d\t\n", ints[0], ints[1], ints[2]);
+        }
+        System.out.println();
+
         //将稀疏数组 --》 恢复成 原始的二维数组
 		/*
 		 *  1. 先读取稀疏数组的第一行，根据第一行的数据，创建原始的二维数组，比如上面的  chessArr2 = int [11][11]
@@ -83,8 +125,8 @@ public class SparseArray {
         System.out.println();
         System.out.println("恢复后的二维数组");
 
-        for (int[] row : chessArr2) {
-            for (int data : row) {
+        for (int[] c2 : chessArr2) {
+            for (int data : c2) {
                 System.out.printf("%d\t", data);
             }
             System.out.println();
