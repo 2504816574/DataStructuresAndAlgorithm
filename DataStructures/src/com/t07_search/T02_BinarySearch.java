8c61ddee031fe8f3b0ac04815b6b5ed9b9453bb9
董升 <2504816574@qq.com>
2021-03-09 16:50:11 +0800

    二分查找（非递归）

--- DataStructures/src/com/t07_search/T02_BinarySearch.java
+++ DataStructures/src/com/t07_search/T02_BinarySearch.java
@@ -9,15 +9,18 @@ import java.util.List;
  */
 public class T02_BinarySearch {
     public static void main(String[] args) {
-        int[] arr = { 1, 8, 10, 89,1000,1000, 1234 };
+        int[] arr = {1, 8, 10, 89, 1000, 1000, 1234};
         // int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
 
         // int resIndex = binarySearch(arr, 0, arr.length - 1, 1000);
         // System.out.println("resIndex=" + resIndex);
 
+        int resIndex = binarySearch(arr, 1000);
+        System.out.println("resIndex=" + resIndex);
         List<Integer> resIndexList = binarySearch2(arr, 0, arr.length - 1, 1000);
         System.out.println("resIndexList=" + resIndexList);
+
     }
 
     // 二分查找算法
@@ -71,11 +74,11 @@ public class T02_BinarySearch {
             // 向左递归
             return binarySearch2(arr, left, mid - 1, findVal);
         } else {
-             // *思路分析
-			 // * 1. 在找到mid 索引值，不要马上返回
-			 // * 2. 向mid 索引值的左边扫描，将所有满足 1000， 的元素的下标，加入到集合ArrayList
-			 // * 3. 向mid 索引值的右边扫描，将所有满足 1000， 的元素的下标，加入到集合ArrayList
-			 // * 4. 将Arraylist返回
+            // *思路分析
+            // * 1. 在找到mid 索引值，不要马上返回
+            // * 2. 向mid 索引值的左边扫描，将所有满足 1000， 的元素的下标，加入到集合ArrayList
+            // * 3. 向mid 索引值的右边扫描，将所有满足 1000， 的元素的下标，加入到集合ArrayList
+            // * 4. 将Arraylist返回
 
             List<Integer> resIndexlist = new ArrayList<Integer>();
             //向mid 索引值的左边扫描，将所有满足 1000， 的元素的下标，加入到集合ArrayList
@@ -102,6 +105,30 @@ public class T02_BinarySearch {
             return resIndexlist;
 
         }
+    }
+
+    /**
+     * 非递归
+     *
+     * @param arr
+     * @param findVal
+     * @return
+     */
+    public static int binarySearch(int[] arr, int findVal) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int mid = ((left + right) / 2);
+            if (arr[mid] == findVal) {
+                return mid;
+            } else if (arr[mid] > findVal) {
+                right = mid - 1;
+            } else {
+                left = mid - 1;
+            }
+
+        }
+        return -1;
     }
 
 }
