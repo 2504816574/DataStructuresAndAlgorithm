d033979fefb1375ba1e3ebcab8f9cb3ede1e0bca
董升 <2504816574@qq.com>
2021-03-13 20:25:45 +0800

    设计哈希表集合

---
+++ LeetCode/src/T000705.java
@@ -0,0 +1,113 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * @auther Ashen One
+ * @Date 2021/3/13
+ * <p>
+ * <p>
+ * 不使用任何内建的哈希表库设计一个哈希集合（HashSet）。
+ * <p>
+ * 实现 MyHashSet 类：
+ * <p>
+ * void add(key) 向哈希集合中插入值 key 。
+ * bool contains(key) 返回哈希集合中是否存在这个值 key 。
+ * void remove(key) 将给定值 key 从哈希集合中删除。如果哈希集合中没有这个值，什么也不做。
+ *  
+ * 示例：
+ * <p>
+ * 输入：
+ * ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+ * [[], [1], [2], [1], [3], [2], [2], [2], [2]]
+ * 输出：
+ * [null, null, null, true, false, null, true, null, false]
+ * <p>
+ * 解释：
+ * MyHashSet myHashSet = new MyHashSet();
+ * myHashSet.add(1);      // set = [1]
+ * myHashSet.add(2);      // set = [1, 2]
+ * myHashSet.contains(1); // 返回 True
+ * myHashSet.contains(3); // 返回 False ，（未找到）
+ * myHashSet.add(2);      // set = [1, 2]
+ * myHashSet.contains(2); // 返回 True
+ * myHashSet.remove(2);   // set = [1]
+ * myHashSet.contains(2); // 返回 False ，（已移除）
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/design-hashset
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class T000705 {
+}
+
+class MyHashSet {
+    private static final int BASE = 769;
+    private LinkedList[] data;
+
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyHashSet() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = new LinkedList<Integer>();
+
+        }
+
+    }
+
+    public void add(int key) {
+        int hash = hash(key);
+        Iterator<Integer> iterator = data[hash].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return;
+            }
+        }
+        data[hash].offerLast(key);
+
+
+    }
+
+    public void remove(int key) {
+        int hash = hash(key);
+        Iterator<Integer> iterator = data[hash].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                data[hash].remove(element);
+                return;
+            }
+        }
+
+    }
+
+    /**
+     * Returns true if this set contains the specified element
+     */
+    public boolean contains(int key) {
+        int hash = hash(key);
+        Iterator<Integer> iterator = data[hash].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int hash(int key) {
+        return key % BASE;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
